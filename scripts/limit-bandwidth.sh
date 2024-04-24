b5e0491d96359f8216b34376c4f8cf205b9aac72
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-24 17:12:19 +0200

    Increase port-reset delay

--- scripts/limit-bandwidth.sh
+++ scripts/limit-bandwidth.sh
@@ -5,6 +5,7 @@ ibportstate -C mlx5_1 10 1 speed 7 # enables 10Gbps
 ibportstate -C mlx5_1 10 1 espeed 30 # disables > 10Gbps
 ibportstate -C mlx5_1 10 1 reset
 
-sleep 5
+echo "Waiting 10 seconds for the port to reset..."
+sleep 10
 
 ibportstate -C mlx5_1 10 1
--- scripts/unlimit-bandwidth.sh
+++ scripts/unlimit-bandwidth.sh
@@ -4,6 +4,7 @@ ibportstate -C mlx5_1 10 1 width 4 # 1x -> 4x
 ibportstate -C mlx5_1 10 1 espeed 31 # enables > 10Gbps
 ibportstate -C mlx5_1 10 1 reset
 
-sleep 5
+echo "Waiting 10 seconds for the port to reset..."
+sleep 10
 
 ibportstate -C mlx5_1 10 1
