47026a11a4ee94e6f3eea599be64a118f1f43bcc
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-24 16:55:05 +0200

    Verbose send

--- send-deployment.sh
+++ send-deployment.sh
@@ -6,6 +6,7 @@ SCRIPT_DIR="$( realpath -sm  "$( dirname "${BASH_SOURCE[0]}" )")"
 cd "$SCRIPT_DIR"
 
 for i in {1..4}; do
+  echo "Sending deployment to w$i"
   scp deployment.zip w$i:~
   ssh w$i "unzip -o deployment.zip -d dsig-artifacts; cd dsig-artifacts/bin; unzip -o bin.zip"
 done
