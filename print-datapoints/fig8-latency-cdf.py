d87567bc8864de02ae81b8a3b49faefb185c5f7c
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-25 23:31:44 +0200

    Fix print-datapoints for fig8

--- print-datapoints/fig8-latency-cdf.py
+++ print-datapoints/fig8-latency-cdf.py
@@ -17,28 +17,24 @@ subbars = [
     ("rverif", "verify"),
 ]
 
-netoffset = parse_ping('logs/pony-ping-dalek-msg8-1.txt')['net'][50] - 0.06
+path = os.path.join("../logs/", figure_name, "eddsa-dalek", "proc1.txt")
+data = parse_ping(path)
+netoffset = data['net'][50]
 
 percentiles = [0.1] + list(range(1,100)) + [99.9]
 
 for h in hashes:
     for scheme in schemes:
-        for k in [12,16,32,64] if 'hors' in scheme else [2,4,8,16]:
-            config = f'{k}-secrets' if 'hors' in scheme else f'{k}-depth'
-            path = os.path.join("../logs/",
-                    figure_name,
-                    f'{h}-hash',
-                    scheme,
-                    config,
-                    "proc1.txt")
-            data = parse_ping(path)
-            print(f'{scheme} with {h} and {config}:')
-            cumul = 0
-            for lat, latname in subbars:
-                print(f'\t- median to {latname}: {data[lat][50]}μs')
-                cumul += data[lat][50]
-            print(f'\t- total percentiles:')
-            for i in percentiles:
-                print(f'\t\t- {i}%: {data['one-way'][i]}μs')
-
-                
+        path = os.path.join("../logs/",
+                figure_name,
+                scheme,
+                "proc1.txt")
+        data = parse_ping(path, network_offset=netoffset)
+        print(f'{scheme}:')
+        cumul = 0
+        for lat, latname in subbars:
+            print(f'\t- median to {latname}: {data[lat][50]}μs')
+            cumul += data[lat][50]
+        print(f'\t- percentiles for total:')
+        for i in percentiles:
+            print(f'\t\t- {i}%: {data['one-way'][i]}μs')
