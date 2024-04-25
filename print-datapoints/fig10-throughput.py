33ea352ac64c14bdc830319750a02944e4ddf699
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-26 00:01:51 +0200

    print-datapoints/fig10

---
+++ print-datapoints/fig10-throughput.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from dsigparser import parse_ping
+import os
+from glob import glob
+
+figure_name="fig10-throughput"
+
+schemes = [
+    'eddsa-dalek',
+    'eddsa-sodium',
+    'dsig',
+]
+
+ingress_types = [
+    "constant-intervals",
+    "random-intervals",
+]
+
+for ing_type in ingress_types:
+    for scheme in schemes:
+        path = os.path.join("../logs/",
+                figure_name,
+                scheme,
+                ing_type,
+                "*-interval-of-*ns',
+                "proc1.txt")
+        paths = glob(path)
+        paths = sorted(paths, key=lambda p: (len(p), p), reverse=True)
+        raw = []
+        for path in paths:
+            data = parse_tput(path)
+            lat = data['one-way'][50]
+            tput = data['tput']
+            raw.append((tput / 1000, lat))
+        xsys = []
+        best_latency = 2**128
+        for tput, lat in sorted(raw, key=lambda xy: xy[0], reverse=True):
+            if(lat > best_latency * 1.1):
+                continue
+            if(lat < best_latency):
+                best_latency = lat
+            xsys.append((tput, lat))
+        xsys.append((0, best_latency))
+        xsys = sorted(xsys, key=lambda xy: xy[0])
+        print(f'tput-latency datapoints for {scheme} with {ing_type}:')
+        for tput, lat in xsys:
+            print(f'\t- {tput}ksig/s, {lat}μs')
