ad12f6de1ab8e8fdd750fcb0e906a02d3a43e9e6
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-26 00:23:32 +0200

    Fix percentile parsing

--- print-datapoints/fig8-latency-cdf.py
+++ print-datapoints/fig8-latency-cdf.py
@@ -28,7 +28,7 @@ for scheme in schemes:
             figure_name,
             scheme,
             "proc1.txt")
-    data = parse_ping(path, network_offset=netoffset)
+    data = parse_ping(path, network_offset=netoffset, filter_tiles=False)
     print(f'{scheme}:')
     cumul = 0
     for lat, latname in subbars:
