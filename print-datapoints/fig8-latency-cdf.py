2d84fee6665cf2e75769d91b8416972f36c457cb
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-25 23:50:25 +0200

    Fix fig8 & fig9 again

--- print-datapoints/fig8-latency-cdf.py
+++ print-datapoints/fig8-latency-cdf.py
@@ -23,18 +23,17 @@ netoffset = data['net'][50]
 
 percentiles = [0.1] + list(range(1,100)) + [99.9]
 
-for h in hashes:
-    for scheme in schemes:
-        path = os.path.join("../logs/",
-                figure_name,
-                scheme,
-                "proc1.txt")
-        data = parse_ping(path, network_offset=netoffset)
-        print(f'{scheme}:')
-        cumul = 0
-        for lat, latname in subbars:
-            print(f'\t- median to {latname}: {data[lat][50]}μs')
-            cumul += data[lat][50]
-        print(f'\t- percentiles for total:')
-        for i in percentiles:
-            print(f'\t\t- {i}%: {data['one-way'][i]}μs')
+for scheme in schemes:
+    path = os.path.join("../logs/",
+            figure_name,
+            scheme,
+            "proc1.txt")
+    data = parse_ping(path, network_offset=netoffset)
+    print(f'{scheme}:')
+    cumul = 0
+    for lat, latname in subbars:
+        print(f'\t- median to {latname}: {data[lat][50]}μs')
+        cumul += data[lat][50]
+    print(f'\t- percentiles for total:')
+    for i in percentiles:
+        print(f'\t\t- {i}%: {data['one-way'][i]}μs')
--- print-datapoints/fig9-message-size.py
+++ print-datapoints/fig9-message-size.py
@@ -21,26 +21,25 @@ path = os.path.join("../logs/", figure_name,
 data = parse_ping(path)
 netoffset = data['net'][50]
 
-for h in hashes:
-    for scheme in schemes:
-        for sz in [8,16,32,64,128,256,512,1024,2048,4096,8192]:
-            path = os.path.join("../logs/",
-                    figure_name,
-                    f'msg-of-{sz}B',
-                    scheme,
-                    "proc1.txt")
-            data = parse_ping(path, network_offset=netoffset)
-            print(f'{scheme} with messages of {sz} B, median total: {data['one-way'][50]}')
-
+for scheme in schemes:
+    for sz in [8,16,32,64,128,256,512,1024,2048,4096,8192]:
         path = os.path.join("../logs/",
                 figure_name,
-                "msg-of-8192B"
+                f'msg-of-{sz}B',
                 scheme,
                 "proc1.txt")
         data = parse_ping(path, network_offset=netoffset)
-        print(f'{scheme} with messages of 8192 B:')
-        cumul = 0
-        for lat, latname in subbars:
-            print(f'\t- median to {latname}: {data[lat][50]}μs')
-            cumul += data[lat][50]
-        print(f'\t- median for total: {data['one-way'][50]}μs')
+        print(f'{scheme} with messages of {sz} B, median total: {data['one-way'][50]}')
+
+    path = os.path.join("../logs/",
+            figure_name,
+            "msg-of-8192B"
+            scheme,
+            "proc1.txt")
+    data = parse_ping(path, network_offset=netoffset)
+    print(f'{scheme} with messages of 8192 B:')
+    cumul = 0
+    for lat, latname in subbars:
+        print(f'\t- median to {latname}: {data[lat][50]}μs')
+        cumul += data[lat][50]
+    print(f'\t- median for total: {data['one-way'][50]}μs')
