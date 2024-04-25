7cf3ee6c4f43325aaa20df8632683bbd8fe67744
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-25 23:25:24 +0200

    Remove msg of size 16 KiB to match fig9

--- experiments/fig9-message-size.sh
+++ experiments/fig9-message-size.sh
@@ -13,7 +13,7 @@ FIGURE_NAME="fig9-message-size"
 
 LOG_BATCH_SIZE=7
 LOG_DEPTH=2
-for MSG_SIZE in {8,16,32,64,128,256,512,1024,2048,4096,8192,16384}; do
+for MSG_SIZE in {8,16,32,64,128,256,512,1024,2048,4096,8192}; do
     "${SCRIPT_DIR}"/test-dsig-wots.sh $FIGURE_NAME/msgs-of-${MSG_SIZE}B/dsig haraka $LOG_BATCH_SIZE $LOG_DEPTH -s $MSG_SIZE
     "${SCRIPT_DIR}"/test-eddsa.sh $FIGURE_NAME/msgs-of-${MSG_SIZE}B/eddsa-dalek dalek -s $MSG_SIZE
     "${SCRIPT_DIR}"/test-eddsa.sh $FIGURE_NAME/msgs-of-${MSG_SIZE}B/eddsa-sodium sodium -s $MSG_SIZE
