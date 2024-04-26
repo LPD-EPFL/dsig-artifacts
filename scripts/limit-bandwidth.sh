4921618caf6f58dff824a9c969108ca0180adfcf
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-26 03:47:29 +0200

    Fix bandwidth scripts

--- scripts/limit-bandwidth.sh
+++ scripts/limit-bandwidth.sh
@@ -1,11 +1,23 @@
+#!/bin/bash
+
 set -u
 
-ibportstate -C mlx5_1 10 1 width 1 # 4x -> 1x
-ibportstate -C mlx5_1 10 1 speed 7 # enables 10Gbps
-ibportstate -C mlx5_1 10 1 espeed 30 # disables > 10Gbps
-ibportstate -C mlx5_1 10 1 reset
+SCRIPT_DIR="$( realpath -sm "$( dirname "${BASH_SOURCE[0]}" )" )"
+source "${SCRIPT_DIR}"/config.sh
 
-echo "Waiting 10 seconds for the port to reset..."
+for i in $(seq 1 $MACHINE_COUNT); do
+    MACHINE=$(machine2ssh machine$i)
+    ssh -o LogLevel=QUIET -t $MACHINE \
+        "ibportstate -C mlx5_1 10 1 width 1 # 4x -> 1x; \
+         ibportstate -C mlx5_1 10 1 speed 7 # enables 10Gbps; \
+         ibportstate -C mlx5_1 10 1 espeed 30 # disables > 10Gbps; \
+         ibportstate -C mlx5_1 10 1 reset"
+done
+
+echo "Waiting 10 seconds for the ports to reset..."
 sleep 10
 
-ibportstate -C mlx5_1 10 1
+for i in $(seq 1 $MACHINE_COUNT); do
+    MACHINE=$(machine2ssh machine$i)
+    ssh -t $MACHINE "ibportstate -C mlx5_1 10 1"
+done
--- scripts/unlimit-bandwidth.sh
+++ scripts/unlimit-bandwidth.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
+
 set -u
 
-ibportstate -C mlx5_1 10 1 width 4 # 1x -> 4x
-ibportstate -C mlx5_1 10 1 espeed 31 # enables > 10Gbps
-ibportstate -C mlx5_1 10 1 reset
+SCRIPT_DIR="$( realpath -sm "$( dirname "${BASH_SOURCE[0]}" )" )"
+source "${SCRIPT_DIR}"/config.sh
 
-echo "Waiting 10 seconds for the port to reset..."
+for i in $(seq 1 $MACHINE_COUNT); do
+    MACHINE=$(machine2ssh machine$i)
+    ssh -o LogLevel=QUIET -t $MACHINE \
+        "ibportstate -C mlx5_1 10 1 width 4 # 1x -> 4x; \
+         ibportstate -C mlx5_1 10 1 espeed 31 # enables > 10Gbps; \
+         ibportstate -C mlx5_1 10 1 reset"
+done
+
+echo "Waiting 10 seconds for the ports to reset..."
 sleep 10
 
-ibportstate -C mlx5_1 10 1
+for i in $(seq 1 $MACHINE_COUNT); do
+    MACHINE=$(machine2ssh machine$i)
+    ssh -t $MACHINE "ibportstate -C mlx5_1 10 1"
+done
