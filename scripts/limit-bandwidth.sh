3cce0cc1f6be5a31ea7134405e89b4dae92661a9
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-26 03:51:30 +0200

    Fix bandwidth script (again)

--- scripts/limit-bandwidth.sh
+++ scripts/limit-bandwidth.sh
@@ -8,9 +8,9 @@ source "${SCRIPT_DIR}"/config.sh
 for i in $(seq 1 $MACHINE_COUNT); do
     MACHINE=$(machine2ssh machine$i)
     ssh -o LogLevel=QUIET -t $MACHINE \
-        "ibportstate -C mlx5_1 10 1 width 1 # 4x -> 1x; \
-         ibportstate -C mlx5_1 10 1 speed 7 # enables 10Gbps; \
-         ibportstate -C mlx5_1 10 1 espeed 30 # disables > 10Gbps; \
+        "ibportstate -C mlx5_1 10 1 width 1; \
+         ibportstate -C mlx5_1 10 1 speed 7; \
+         ibportstate -C mlx5_1 10 1 espeed 30; \
          ibportstate -C mlx5_1 10 1 reset"
 done
 
--- scripts/unlimit-bandwidth.sh
+++ scripts/unlimit-bandwidth.sh
@@ -8,8 +8,8 @@ source "${SCRIPT_DIR}"/config.sh
 for i in $(seq 1 $MACHINE_COUNT); do
     MACHINE=$(machine2ssh machine$i)
     ssh -o LogLevel=QUIET -t $MACHINE \
-        "ibportstate -C mlx5_1 10 1 width 4 # 1x -> 4x; \
-         ibportstate -C mlx5_1 10 1 espeed 31 # enables > 10Gbps; \
+        "ibportstate -C mlx5_1 10 1 width 4; \
+         ibportstate -C mlx5_1 10 1 espeed 31; \
          ibportstate -C mlx5_1 10 1 reset"
 done
 
