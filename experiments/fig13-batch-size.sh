246e8b7ff5895e0551d66d18f8efdb744a3b15e1
Clement Burgelin (Zyf) <clementburgelin@gmail.com>
2024-04-26 05:51:27 +0200

    Add back experiment with batch of 1

--- experiments/fig13-batch-size.sh
+++ experiments/fig13-batch-size.sh
@@ -14,7 +14,7 @@ FIGURE_NAME="fig13-batch-size"
 "${SCRIPT_DIR}"/test-eddsa.sh dsig-latency/dalek-reference dalek
 
 DEPTH=2
-for LOG_BATCH_SIZE in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; do
+for LOG_BATCH_SIZE in {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; do
     BATCH_SIZE=$(( 2 ** $LOG_BATCH_SIZE ))
     "${SCRIPT_DIR}"/test-dsig-wots.sh $FIGURE_NAME/latency/batchsize-of-$BATCH_SIZE haraka $LOG_BATCH_SIZE $DEPTH
     "${SCRIPT_DIR}"/test-cpu-tput.sh $FIGURE_NAME/cpu-tput/batchsize-of-$BATCH_SIZE $LOG_BATCH_SIZE $DEPTH
